9477c0ff7704b61c50216115a6879e0050bee93e
Sumedh Hajare <hajaresumedh1000@gmail.com>
2025-11-12 14:09:09 +0530

    initial: project Controlflow

---
+++ src/com/controlflow_statments/Keywords.java
@@ -0,0 +1,20 @@
+package com.controlflow_statments;
+
+public class Keywords {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("if");
+        System.out.println("else");
+        System.out.println("switch");
+        System.out.println("case");
+        System.out.println("default");
+        System.out.println("for");
+        System.out.println("while");
+        System.out.println("do");
+        System.out.println("break");
+        System.out.println("continue");
+        System.out.println("return");
+	}
+}
